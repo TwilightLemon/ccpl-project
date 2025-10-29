6a1952fbbd9c4e2f04b7452c831921e5a162698c
TwilightLemon <cz241126@live.com>
2025-10-29 23:03:17 +0800

    ？

--- ccpl/test/array.m
+++ ccpl/test/array.m
@@ -5,7 +5,13 @@ char name_list[5][10];
 
 int main(){
     struct A a1;
+    int x,y;
+    x=2;
+    y=3;
     a1.id=9;
-    a1.name_list[2][3]='x';
+    char *k;
+    k=a1.name_list + x * 40 + y * 8;
+    *k='x';
+    output a1.name_list[2][3]; # 应该输出x
     return 0;
 }
